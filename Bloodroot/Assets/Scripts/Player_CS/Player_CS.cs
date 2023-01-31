7acc60df146868a22695049500a07480eaf4f47d
BlinkLudus <conor.sheppard@hotmail.com>
2023-01-31 21:39:06 +0000

    GameOver scene

--- Bloodroot/Assets/Scripts/Player_CS/Player_CS.cs
+++ Bloodroot/Assets/Scripts/Player_CS/Player_CS.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
 
 public class Player_CS : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
+    public static bool isGameOver;
 
     public HealthBar healthBar;
 
     void Start()
     {
+        isGameOver = false;
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
@@ -21,12 +25,20 @@ public class Player_CS : MonoBehaviour
         {
             TakeDamage(20);
         }
+
+        if (isGameOver)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
         healthBar.SetHealth(currentHealth);
+        if(currentHealth <= 0)
+        {
+            isGameOver = true;
+        }
     }
 }
