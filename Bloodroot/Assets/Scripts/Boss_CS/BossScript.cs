144e91e51f8a28fb02556e918d03c1ee735a60e2
CONOR SHEPPARD <conor.sheppard@hotmail.com>
2023-02-01 11:49:43 +0000

    Boss script

---
+++ Bloodroot/Assets/Scripts/Boss_CS/BossScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+
+
+    public Transform Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, Player.position) < stoppingDistance && Vector2.Distance(transform.position, Player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, Player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, -speed * Time.deltaTime);
+        }
+    }
+}
